3e1e73ee3b3048cf1fce0f40406512d47bbae5be
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-13 10:34:14 +0300

    put on the while loop

--- python_app.py
+++ python_app.py
@@ -19,35 +19,36 @@ def mul(a,b):
 def div(a,b):
     answer = a / b
     print(str(a) + " / " + str(b) + " = " + str(answer))
-    
-print("A. Addition")
-print("B. Subtraction")
-print("C. Multiplication")
-print("D. Division")
-print("E. Exit")
 
-choice = input("input your choice: ")
+while True:
+    print("A. Addition")
+    print("B. Subtraction")
+    print("C. Multiplication")
+    print("D. Division")
+    print("E. Exit")
 
-if choice == "a" or choice == "A":
-    print("Addition")
-    a = int (input("input first number: "))
-    b = int (input("input second number: "))
-    add(a, b)
-elif choice == "b" or choice == "B":
-    print("Subtraction")
-    a = int (input("input first number: "))
-    b = int (input("input second number: "))
-    sub(a, b)
-elif choice == "c" or choice == "C":
-    print("Multiplication")
-    a = int (input("input first number: "))
-    b = int (input("input second number: "))
-    mul(a, b)
-elif choice == "d" or choice == "D":
-    print("Division")
-    a = int (input("input first number: "))
-    b = int (input("input second number: "))
-    div(a, b)
-elif choice == "e" or choice == "E":
-    print("Program Ended")
-    quit()
+    choice = input("input your choice: ")
+
+    if choice == "a" or choice == "A":
+        print("Addition")
+        a = int (input("input first number: "))
+        b = int (input("input second number: "))
+        add(a, b)
+    elif choice == "b" or choice == "B":
+        print("Subtraction")
+        a = int (input("input first number: "))
+        b = int (input("input second number: "))
+        sub(a, b)
+    elif choice == "c" or choice == "C":
+        print("Multiplication")
+        a = int (input("input first number: "))
+        b = int (input("input second number: "))
+        mul(a, b)
+    elif choice == "d" or choice == "D":
+        print("Division")
+        a = int (input("input first number: "))
+        b = int (input("input second number: "))
+        div(a, b)
+    elif choice == "e" or choice == "E":
+        print("Program Ended")
+        quit()
